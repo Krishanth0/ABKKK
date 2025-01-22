a0017538a87c82b50dab9d1deed63461ea10aba1
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2025-01-22 17:59:36 +0100

    ++

--- src/components/Anneback.jsx
+++ src/components/Anneback.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import face from '../assets/blue6.png'; 
 
 const Anneback = () => {
   const fullText = `What is Lorem Ipsum?
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.`;
 
   const [displayedText, setDisplayedText] = useState('');
 
@@ -21,8 +22,13 @@ const Anneback = () => {
   }, [fullText]);
 
   return (
-    <section className="bg-[#E2E2E1] h-screen flex items-center justify-center p-8">
-      <div className="text text-black text-lg leading-relaxed max-w-3xl font-serif">
+    <section className="bg-[#E2E2E1] h-screen flex items-center justify-between p-8">
+
+      <div className="flex-1 flex justify-center items-center">
+        <img src={face} alt="Face" className='h-screen w-screen' />
+      </div>
+
+      <div className=" text text-black text-lg leading-relaxed max-w-[500px] font-serif">
         {displayedText}
       </div>
     </section>
